c9e6a53a483017d4296e4a0411f81b1f930c56cf
saroko-dnd <isaroko@mail.ru>
2018-02-22 04:07:34 +0200

    function was successfully implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function solveEquation(equation) {
-  // your implementation
-}
+module.exports = equation => {
+    const coefficients = equation
+        .replace(/ /g, "")
+        .match(/[^\^]\d+/g)
+        .map(str => +str);
+    const discriminant =
+        coefficients[1] ** 2 - 4 * coefficients[0] * coefficients[2];
+
+    return [
+        Math.round(-coefficients[1] + discriminant ** (1 / 2)) /
+            (2 * coefficients[0]),
+        Math.round(-coefficients[1] - discriminant ** (1 / 2)) /
+            (2 * coefficients[0])
+    ].sort((a, b) => a - b);
+};
